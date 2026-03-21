7a6242aa2753846b618d1b537ee4648277f46423
tanmay493 <tanmaylanjewar24874@gmail.com>
2026-03-21 13:01:00 +0530

    render

--- Responses/render/project/app/views.py
+++ Responses/render/project/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(req):
+    data={
+        "name":"tom",
+        "age":22
+    }
+    return render(req,'landing.html',data)
