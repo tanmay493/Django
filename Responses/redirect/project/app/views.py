6db56f0b2ca28b1630956511bf1df3ba14d55fa3
tanmay493 <tanmaylanjewar24874@gmail.com>
2026-03-22 17:05:18 +0530

    redirect

--- Responses/redirect/project/app/views.py
+++ Responses/redirect/project/app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def my_redirect1(req):
+
+    return redirect('first')  #for internal redirect
+
+def first(req):
+    return render(req,'landing.html')
+# ----------------------------------------
+# def my_redirect1(req):
+
+#      return redirect('https://www.google.com/')  # //for external css
+# -------------------------------------------------------------
+
+
+
+
+
--- Responses/redirect/project/project/urls.py
+++ Responses/redirect/project/project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.my_redirect1,name="my_redirect1"),
+    path('first/',views.first,name="first"),
+    
 ]
