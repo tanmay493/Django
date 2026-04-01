0af2635957b7f35489c044922cbb3b1c9bbf67e3
tanmay493 <tanmaylanjewar24874@gmail.com>
2026-04-01 09:41:04 +0530

    static_outside_app

--- static_outside_app/project/app/views.py
+++ static_outside_app/project/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(req):
+    data={'name':'tom','city':'bhopal','p':'this is django class'}
+    return render(req,'home.html',data)
+
+
+def about(req):
+
+    return render(req,'about.html')
