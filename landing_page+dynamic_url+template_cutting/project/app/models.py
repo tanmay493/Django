09a046faba13653de131c222fe8018d2a0e2b267
tanmay493 <tanmaylanjewar24874@gmail.com>
2026-04-13 13:19:15 +0530

    creating_database_by_queries

--- landing_page+dynamic_url+template_cutting/project/app/models.py
+++ landing_page+dynamic_url+template_cutting/project/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    Name=models.CharField()
+    Email=models.EmailField()
+    Contact=models.IntegerField(null=True)
+    Gender=models.CharField(null=True)
+    Courses=models.CharField(null=True)
+    Qualification=models.CharField(null=True)
+    Resume=models.FileField(null=True)
+    Profile_Pic=models.ImageField(null=True)
+    DOB=models.DateField(null=True)
+    Registered_date_time=models.DateTimeField(null=True)
+    Registered_time=models.TimeField(null=True)
+    Password=models.CharField(null=True)
+    Confirm_Password=models.CharField(null=True)
+
+
+
--- landing_page+dynamic_url+template_cutting/project/app/views.py
+++ landing_page+dynamic_url+template_cutting/project/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 # Create your views here.
 
 def landing(req):
@@ -44,9 +45,36 @@ def feature(req):
     return render (req,'feature.html',{'xyz':data})  
 
 def reg_data(req):
-    print('hello')
-    print(req.GET)  
-    print(req.POST) 
-    print(req.FILES) 
-    print(req.COOKIES)
-    print(req.META)
+    # print('hello')
+    # print(req.GET)  
+    # print(req.POST) 
+    # print(req.FILES) 
+    # print(req.COOKIES)
+    # print(req.META)
+
+    if req.method=='POST':
+        n=req.POST.get('name')
+        e=req.POST.get('email')
+        c=req.POST.get('contact')
+        g=req.POST.get('gender')
+        co=req.POST.getlist('courses')
+        q=req.POST.get('qualification')
+        r=req.POST.get('resume')
+        p=req.POST.get('profile_pic')
+        d=req.POST.get('dob')
+        rdt=req.POST.get('registered_date_time')
+        rt=req.POST.get('registered_time')
+        p=req.POST.get('password')
+        cp=req.POST.get('confirm_password')
+        user=Student.objects.filter(Email=e)
+        if user:
+            msg='Email already exist'
+            return render(req,'register.html',{'msg': msg})
+        else:
+            if p==cp:
+                Student.objects.create(Name=n,Email=e,Contact=c,Gender=g,Courses=co,Qualification=q,Resume=r,Password=p,Confirm_Password=cp,Registered_date_time=rdt,Registered_time=rt,DOB=d,Profile_Pic=p)    
+                msg="registration successful"
+                return render(req,'login.html',{'msg1':msg})
+            else:
+                msg='password & confirm_password does not match'    
+                return render(req,'register.html',{'msg2':msg,'n':n,'e':e})
